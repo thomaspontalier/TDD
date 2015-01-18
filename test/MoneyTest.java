bbcc35a1507b7563e1110aaa2ccc06158aef90c6
thomaspontalier <thomas.pontalier@gmail.com>
2015-01-19 00:29:06 +0100

    Chapitre 5

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class DollarTest {
+public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five= new Dollar(5);
@@ -16,5 +16,12 @@ public class DollarTest {
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five= new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 }
