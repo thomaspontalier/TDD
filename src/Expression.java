cb759a9e56670e2e96f7bb0a1ce45a40782163ad
thomaspontalier <thomas.pontalier@gmail.com>
2015-01-20 00:07:59 +0100

    Chapitre 13

--- src/Expression.java
+++ src/Expression.java
@@ -2,4 +2,5 @@
  * Created by Thomas on 19/01/2015.
  */
 public interface Expression {
+    Money reduce(String to);
 }
--- src/Money.java
+++ src/Money.java
@@ -28,7 +28,11 @@ class Money implements Expression{
     }
 
     Expression plus(Money addend) {
-        return new Money(amount + addend.amount, currency);
+        return new Sum(this, addend);
+    }
+
+    public Money reduce(String to) {
+        return this;
     }
 
     protected String currency;
