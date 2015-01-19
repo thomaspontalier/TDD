a6effb4c2360a0c4b70911d5f298c94e923b445b
thomaspontalier <thomas.pontalier@gmail.com>
2015-01-20 00:17:33 +0100

    Chapitre 14

--- src/Bank.java
+++ src/Bank.java
@@ -1,8 +1,22 @@
+import java.util.Hashtable;
+
 /**
  * Created by Thomas on 19/01/2015.
  */
 public class Bank {
+    private Hashtable rates= new Hashtable();
+
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    int rate(String from, String to) {
+        if (from.equals(to)) return 1;
+        Integer rate= (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
+    }
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
     }
 }
--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -64,4 +64,20 @@ public class MoneyTest {
         Money result= bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank= new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result= bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    public void testArrayEquals() {
+        assertEquals(new Object[] {"abc"}, new Object[] {"abc"});
+    }
+
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
 }
