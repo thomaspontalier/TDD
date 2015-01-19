fc0c2a0dbf7051d2dce408d4dad4d09498fe6daa
thomaspontalier <thomas.pontalier@gmail.com>
2015-01-20 00:25:11 +0100

    Chapitre 15

--- src/Expression.java
+++ src/Expression.java
@@ -2,5 +2,7 @@
  * Created by Thomas on 19/01/2015.
  */
 public interface Expression {
-    Money reduce(Bank bank, String to);;
+    Money reduce(Bank bank, String to);
+
+    Expression plus(Expression addend);
 }
--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -73,11 +73,23 @@ public class MoneyTest {
         assertEquals(Money.dollar(1), result);
     }
 
+    @Test
     public void testArrayEquals() {
         assertEquals(new Object[] {"abc"}, new Object[] {"abc"});
     }
 
+    @Test
     public void testIdentityRate() {
         assertEquals(1, new Bank().rate("USD", "USD"));
+    }
+
+    @Test
+    public void testMixedAddition() {
+        Expression fiveBucks= Money.dollar(5);
+        Expression tenFrancs= Money.franc(10);
+        Bank bank= new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result= bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
     }
 }
